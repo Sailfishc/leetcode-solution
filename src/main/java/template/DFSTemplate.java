abb83ca70a1e35ca6512737e65e846c30afbad1c
cheng.zhang <mr.cheng94@foxmail.com>
2019-11-25 11:14:44 +0800

    Initial commit

---
+++ src/main/java/template/DFSTemplate.java
@@ -0,0 +1,10 @@
+package template;
+
+/**
+ * @author sailfish
+ * @create 2019-11-25-11:10 上午
+ */
+public class DFSTemplate {
+
+
+}
---
+++ src/main/java/template/UnionFindTemplate.java
@@ -0,0 +1,10 @@
+package template;
+
+/**
+ * 并查集模板
+ * @author sailfish
+ * @create 2019-11-25-11:11 上午
+ */
+public class UnionFindTemplate {
+
+}
