c3b463931454c3058bf2fa8ff340f8a6178e18a1
sailfish <sailfishcc@gamil.com>
2020-03-26 16:33:33 +0800

    add sikytuib

---
+++ src/main/java/algorithm/L0283_MoveZeros/Solution3.java
@@ -0,0 +1,26 @@
+package algorithm.L0283_MoveZeros;
+
+/**
+ * @author sailfish
+ * @create 2020-03-26-1:52 PM
+ */
+public class Solution3 {
+
+
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return;
+        }
+        int nonZero = 0;
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != 0) {
+                nums[nonZero++] = nums[index];
+            }
+        }
+
+        while (nonZero < nums.length) {
+            nums[nonZero++] = 0;
+        }
+    }
+}
